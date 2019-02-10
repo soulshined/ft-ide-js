73c9b87ad5a80c1e8a025fd864e71c0b5b717e37
David Freer <davidwfreer@gmail.com>
2019-02-10 16:20:06 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# ft-ide-js
+
+FT-IDE is a simple, clean browser 'IDE' layout for code snippets. **It is simply a layout and not an actual IDE.* 
+
+Styled with the power of CSS and customized with the power of JS
+
+## Features
+
+- Responsive 
+- Customizations
+  - choose font for the explorer or code 
+  - syntax highlighting 
+  - set 'root path' of which 'file' is selected first
+  - breadcrumb options
+  - and more!
+
+There are **minimal** `!important` statements in the CSS markup so you can even expand on the layout and create your own theme. Out-of-the-box support for a light or dark theme.  
+
+---
+
+The goal of ft-ide is to be able to expand on snippets in a way that is project friendly for browser users. For example, if you want to share with users how your actual website layout is, the following could demonstrate that:
+
+![overview image](./docs/images/overview.PNG?raw=true "Overview")
+
+# Using
+
+**Browser**
+
+1. Add a simple script tag anywhere in your document:
+```html
+  <head>
+    ...
+    <script src="https://cdn.jsdelivr.net/gh/soulshined/ft-ide-js/<version number>/src/ft-ide.min.js"></script>
+    ...
+  </head>
+```
+> You can find version numbers & differences in <a href="https://github.com/soulshined/ft-ide-js/releases" target="_blank">releases</a>
+
+2. Add a div in your markup with an id:
+```html
+  ...
+  <div id="ft-ide1"></div>
+  ...
+```
+
+3. Declare a new FTIDE object and pass the div's id and tree structure:
+```js
+new FTIDE('ft-ide1', {
+  'css' : {
+    'main.css' : '<main css content goes here>',
+    'styles.css' : '<styles css content goes here>'
+  },
+  'js' : {
+    'main.js' : '<main js content goes here>'
+  },
+  'index.html' : '<index html content goes here>'
+})
+```
+
+# Demos & Usage:
+
+https://soulshined.github.io/ft-ide-js/
---
+++ docs/js/main.js
@@ -0,0 +1,216 @@
+new FTIDE('ide-1', {
+  'main.js' : `let opts = new FTIDEOptions({
+  theme : 'dark'
+});
+
+new FTIDE('ide-1', {
+  'main.js' : <omitted to prevent confusion>,
+  'index.html' : \`
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="main.js"></script>
+</head>
+<body>
+  <div id="ide-1"></div>
+</body>
+</html>
+\`
+}, opts)
+  `,
+  'index.html' : `<!DOCTYPE html>
+<html>
+<head>
+  <script src="main.js"></script>
+</head>
+<body>
+  <div id="ide-1"></div>
+</body>
+</html>
+  `
+}, {
+  theme: 'dark'
+})
+new FTIDE('ide-2', {
+  'css' : {
+  
+  },
+  'js' : {
+    'main.js': `new FTIDE('ide-2', {
+  'css' : {
+  },
+  'js' : {
+    'main.js' : <omitted to prevent confusion>
+  },
+  'index.html' : \`
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="js/main.js"></script>
+</head>
+<body>
+  <p>Hello World! Check out my code here:</p>
+  <div id="ide-2"></div>
+</body>
+</html>
+\`,
+  'readme.md' : \`
+This examples uses:
+- syntax highlighting (built-in support)
+- light theme
+\`
+}, {
+  theme: 'light',
+  syntaxHighlighting: {
+    highlight: true
+  }
+})
+  `
+  },
+  'index.html': `<!DOCTYPE html>
+<html>
+<head>
+  <script src="js/main.js"></script>
+</head>
+<body>
+  <p>Hello World! Check out my code here:</p>
+  <div id="ide-2"></div>
+</body>
+</html>
+  `,
+  'readme.md' : `This examples uses:
+- syntax highlighting (built-in support)
+- light theme
+  `
+}, {
+  theme: 'light',
+  syntaxHighlighter : {
+    highlight: true,
+  }
+})
+new FTIDE('ide-3', {
+  'css': {
+
+  },
+  'js': {
+    'main.js': `new FTIDE('ide-3', {
+  'css' : {
+  },
+  'js' : {
+    'main.js' : <omitted to prevent confusion>
+  },
+  'index.html' : \`
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="js/main.js"></script>
+</head>
+<body>
+  <div id="ide-3"></div>
+</body>
+</html>
+\`,
+  'readme.md' : \`This examples uses:
+- syntax highlighting
+- light theme
+\`
+}, {
+  theme: 'light',
+  syntaxHighlighting: {
+    highlight: true
+  }
+})
+  `
+  },
+  'index.html': `<!DOCTYPE html>
+<html>
+<head>
+  <script src="js/main.js"></script>
+</head>
+<body>
+  <div id="ide-3"></div>
+</body>
+</html>
+  `,
+  'readme.md': `This examples uses:
+- syntaxHighlighting (show difference for dark theme)
+- dark theme
+- compact explorerStyle
+- showBreadcrumbs
+  `
+}, {
+  theme: 'dark',
+  explorerStyle: 'compact',
+  showBreadcrumbs: 'always',
+  syntaxHighlighter : {
+    highlight: true
+  }
+})
+new FTIDE('ide-4', {
+  'css': {
+
+  },
+  'js': {
+    'main.js': `new FTIDE('ide-4', {
+  'css' : {
+  },
+  'js' : {
+    'main.js' : <omitted to prevent confusion>
+  },
+  'index.html' : \`
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="js/main.js"></script>
+</head>
+<body>
+  <div id="ide-4"></div>
+</body>
+</html>
+\`,
+  'readme.md' : \`
+This examples uses:
+- syntaxHighlighting 
+- changed ide font
+- changed code font
+- changed root path to index.html
+- expandedFolders
+\`
+}, {
+  syntaxHighlighting: {
+    highlight: true
+  },
+  expandFolders: true,  
+  codeFontFamily: 'Anonymous Pro',
+  ideFontFamily: 'Staatliches',
+  rootPath: 'index.html'
+})
+  `
+  },
+  'index.html': `<!DOCTYPE html>
+<html>
+<head>
+  <script src="prism.js" data-manual></script>
+  <script src="js/main.js"></script>
+</head>
+<body>
+  <div id="ide-4"></div>
+</body>
+</html>
+  `,
+  'readme.md': `This examples uses:
+- syntaxHighlighting 
+- changed ide font
+- changed code font
+- changed root path to index.html
+- expandedFolders
+  `
+}, {
+  syntaxHighlighter: {
+      highlight: true
+  },
+  expandFolders: true,
+  codeFontFamily: 'Anonymous Pro',
+  ideFontFamily: 'Staatliches',
+  rootPath: 'index.html'
+})
